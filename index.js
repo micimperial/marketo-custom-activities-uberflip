c02021d1cb522268589c6772217e177ec366fd86
Michael Imperial <michael.imperial@uberflip.com>
2016-11-15 15:43:53 -0500

    .

--- index.js
+++ index.js
@@ -4,18 +4,13 @@ var url = require('url')
 var pkg = require('./package')
 var MongoClient = require('mongodb').MongoClient
 var app = express()
-
-// Default Variables
+	// Default Variables
 var clientId = "";
 var clientSecret = "";
 var token = "";
 var urlId = ""; //Get from cookie
-
-
-
 // Connection URL
 var dbUrl = 'mongodb://admin:admin@ds061278.mlab.com:61278/aweber';
-
 //Check DB for defaults
 MongoClient.connect(dbUrl, function (err, db) {
 	assert.equal(null, err);
@@ -25,7 +20,19 @@ MongoClient.connect(dbUrl, function (err, db) {
 			apiToken = res.apiToken;
 			apiTokenSecret = res.apiTokenSecret;
 		}
-		console.log (apiTokenSecret)
+		console.log(apiTokenSecret)
 		db.close();
 	});
+});
+//Get Fields
+app.post('/get-fields', function (req, res) {
+	console.dir(req);
+	res.status(200)
+	res.end()
+});
+//Submit
+app.post('/submit', function (req, res) {
+	console.dir(req);
+	res.status(200)
+	res.end()
 });
