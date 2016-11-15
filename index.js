05adb60891b77dd337f2b026edd5785796585544
Michael Imperial <michael.imperial@uberflip.com>
2016-11-15 16:59:32 -0500

    .

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var url = require('url')
-var pkg = require('./package')
+var express = require('express');
+var bodyParser = require('body-parser');
+var url = require('url');
+var pkg = require('./package');
 var MongoClient = require('mongodb').MongoClient
 	, assert = require('assert');
-	// Default Variables
-//var clientId = "";
-//var clientSecret = "";
-//var token = "";
-//var urlId = "";
-// Connection URL
-// var dbUrl = 'mongodb://admin:admin@ds061278.mlab.com:61278/aweber';
-//Check DB for defaults
-//MongoClient.connect(dbUrl, function (err, db) {
+var url = 'mongodb://admin:admin@ds061278.mlab.com:61278/aweber';
+// Defaults
+var app = express();
+var apiId;
+var apiToken;
+var apiSecret;
+var apiUrl;
+//Check DB
+//MongoClient.connect(url, function (err, db) {
 //	assert.equal(null, err);
 //	db.collection("tokens").findOne({}, function (err, res) {
 //		assert.equal(null, err);
@@ -24,42 +24,55 @@ var MongoClient = require('mongodb').MongoClient
 //		db.close();
 //	});
 //});
-//Get Fields
-// app.post('/get-fields', function (req, res) {
-//	console.dir(req);
-//	var fields = [
-//			{
-//				'display_name': 'Email Address'
-//				, 'html_name': 'email_address'
-//				, 'control_type': 'text'
-//				, 'required': true
-//				, 'active': true
-//				, 'locked': true
-//      }
-//			, {
-//				'display_name': 'First Name'
-//				, 'html_name': 'first_name'
-//				, 'control_type': 'text'
-//				, 'required': false
-//				, 'active': true
-//				, 'locked': false
-//      }
-//			, {
-//				'display_name': 'Last Name'
-//				, 'html_name': 'last_name'
-//				, 'control_type': 'text'
-//				, 'required': false
-//				, 'active': true
-//				, 'locked': false
-//      }
-//    ]
-//	res.json(fields)
-//	res.status(200)
-//	res.end()
-// });
-//Submit
-//app.post('/submit', function (req, res) {
-//	console.dir(req);
-//	res.status(200)
-//	res.end()
-//});
+app.use(bodyParser.json())
+app.get('/', function (req, res) {
+	res.send(pkg.name + ' listening on ' + port)
+})
+app.listen(port, function () {
+	console.log(pkg.name + ' listening on port ' + port)
+})
+app.post('/get-fields', function (req, res) {
+
+		var fields = [
+			{
+				'display_name': 'Email Address'
+				, 'html_name': 'email_address'
+				, 'control_type': 'text'
+				, 'required': true
+				, 'active': true
+				, 'locked': true
+      }
+			, {
+				'display_name': 'First Name'
+				, 'html_name': 'first_name'
+				, 'control_type': 'text'
+				, 'required': false
+				, 'active': true
+				, 'locked': false
+      }
+			, {
+				'display_name': 'Last Name'
+				, 'html_name': 'last_name'
+				, 'control_type': 'text'
+				, 'required': false
+				, 'active': true
+				, 'locked': false
+      }
+    ]
+
+		res.json(fields)
+		res.status(200)
+		res.end()
+	}, function (error) {
+		if (error.error) {
+			res.json({
+				'errors': [{
+					'message': error.error
+					, 'code': error.code
+				}]
+			})
+		}
+		res.status(500)
+		res.end()
+	})
+})
